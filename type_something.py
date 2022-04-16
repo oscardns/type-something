6011e9a545dac3016dd3a4243e407ac3f9199493
Oscar Dionisio Nunez Siri <oscardns96@gmail.com>
2022-04-16 00:07:58 -0400

    Making the code easier, and fixing possible user input errors

--- type_something.py
+++ type_something.py
@@ -1,24 +1,23 @@
 import sys
 import time
 import pyautogui as spam
-from logging import exception
 
+# Main variables
 messages1 = [".daily", ".rep 781484526043398165"]
 messages2 = [".daily", ".rep 618154623722848257", ".reminder 12h Do the daily and the rep"]
+time_given = 0
 
 def input_time():
-    # Main variables
-    time_given = 0
-    
     try:
         # Give time to the user to select the application
         time_given = int(input("How many seconds you want to give to the user?: "))
         time.sleep(time_given)
     except:
-        exception("The time given was not an amount of seconds!")
+        print("The time given was not an amount of seconds!")
+        quit()
 
 def type_maki(messages_array):
-    # Main variables
+    # Helper variables
     i = 0
     
     try:
@@ -29,7 +28,7 @@ def type_maki(messages_array):
             time.sleep(4)
             i += 1
     except:
-        exception("Something went wrong!")
+        print("Something went wrong!")
     time.sleep(5)
 
 if __name__ == '__main__':
